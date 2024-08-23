1f67fc963cd38c5702b0aa57f32d719b6d5b7a58
Solotole <solomontoleak47@gmail.com>
2024-08-23 03:06:21 +0100

    minimum operations as possible

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,17 +6,17 @@
 def minOperations(n):
     """ minimum operations- Copy  All and paste
         to reach the required number of H
-    """ 
+    """
     if n <= 1:
         return 0
-    
+
     ops = 0
     divisor = 2
-    
+
     while n > 1:
         while n % divisor == 0:
             ops += divisor
             n //= divisor
         divisor += 1
-    
+
     return ops
