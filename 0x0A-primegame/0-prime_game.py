94321572520ff4016e71f644d190e5c02baa7328
Solotole <solomontoleak47@gmail.com>
2024-10-16 09:48:59 +0100

    The Gamer Theory

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,44 +1,40 @@
 #!/usr/bin/python3
+"""Prime Game"""
 
 
 def isWinner(x, nums):
-    def is_prime(num):
-        if num < 2:
-            return False
-        for i in range(2, int(num ** 0.5) + 1):
-            if num % i == 0:
-                return False
-        return True
+    """the gamer theory algorithm
+    """
+    def sieve(n):
+        """sieving and determining if prime
+        """
+        primes = [True] * (n + 1)
+        p = 2
+        while (p * p <= n):
+            if primes[p]:
+                for i in range(p * p, n + 1, p):
+                    primes[i] = False
+            p += 1
+        return [p for p in range(2, n + 1) if primes[p]]
 
-    def find_primes(n):
-        primes = []
-        for i in range(2, n + 1):
-            if is_prime(i):
-                primes.append(i)
-        return primes
-
-    def get_winner(round_nums):
-        primes = find_primes(max(round_nums))
-        maria_wins = 0
-        for n in round_nums:
-            if n in primes:
-                maria_wins += 1
-        if maria_wins > len(round_nums) // 2:
-            return "Maria"
-        elif maria_wins < len(round_nums) // 2:
-            return "Ben"
+    max_n = max(nums)
+    primes_up_to_max_n = sieve(max_n)
+    prime_count = [0] * (max_n + 1)
+    for i in range(1, max_n + 1):
+        prime_count[i] = prime_count[i - 1]
+        if i in primes_up_to_max_n:
+            prime_count[i] += 1
+    maria_wins = 0
+    ben_wins = 0
+    for n in nums:
+        if prime_count[n] % 2 == 1:
+            maria_wins += 1
         else:
-            return None
+            ben_wins += 1
 
-    winners = []
-    for round_nums in nums:
-        winners.append(get_winner(round_nums))
-
-    maria_wins = winners.count("Maria")
-    ben_wins = winners.count("Ben")
     if maria_wins > ben_wins:
         return "Maria"
-    elif maria_wins < ben_wins:
+    elif ben_wins > maria_wins:
         return "Ben"
     else:
         return None
